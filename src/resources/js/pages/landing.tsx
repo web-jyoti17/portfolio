b8373a22e53a0700905361ab82d922e47f0520a5
Jyoti <jyoti@192.168.1.6>
2025-03-24 23:37:34 +0530

    Initial commit

---
+++ src/resources/js/pages/landing.tsx
@@ -0,0 +1,20 @@
+import AppLayout from '@/layouts/landing-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+];
+
+export default function Dashboard() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Dashboard" />
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+ss            </div>
+        </AppLayout>
+    );
+}
